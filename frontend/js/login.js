c0d140a3fd6cdfce503291cb1ad07eba976d337f
Sayem-ELE <sayem.libra@gmail.com>
2025-08-04 20:23:29 +0600

    passing info from frontend to backend

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -10,5 +10,25 @@ const handleLogin = () => {
    { userId: userId,
      password: password,
    };
-   console.log(user);
+   
+   fetchUserInfo(user);
+};
+
+const fetchUserInfo = async (user) => {
+  let data;
+  try {
+    const res = await fetch("http://localhost:5000/getUserInfo", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    data = await res.json();
+  } catch (err) {
+    console.log("Error connecting to the server: ", err);
+  } finally {
+    console.log("User info from server: ", data);
+  }
 };
