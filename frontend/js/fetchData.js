06baf7c30ba604c6183862e9ca4f8e0353ffb97c
Sayem-ELE <sayem.libra@gmail.com>
2025-08-07 03:18:51 +0600

    using inner html to send data dynamically

--- frontend/js/fetchData.js
+++ frontend/js/fetchData.js
@@ -3,8 +3,51 @@ const fetchAllPosts = async () => {
     const res = await fetch("http://localhost:5000/getAllPosts");
     const data = await res.json();
     console.log("data coming from server: ", data);
+    showAllPosts(data);
   } catch (err) {
     console.log("error fetching data from server: ", err);
   }
 };
+const showAllPosts= (allPosts) => {
+  const postContainer = document.getElementById("post-container");
+
+  postContainer.innerHTML = "";
+
+  allPosts.forEach ((post) => {
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+
+    postDiv.innerHTML = `
+    <div class="post-header">
+            <div class="post-user-image">
+              <img src=${post.postedUserImage}
+              />
+            </div>
+
+            <div class="post-username-time">
+              <p class="post-username">${post.postedUserName}</p>
+              <div class="posted-time">
+                <span>5${post.postedTime}</span>
+                <span>hours ago</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="post-text">
+            <p class="post-text-content">
+              ${post.postedText}
+            </p>
+          </div>
+
+          <div class="post-image">
+            <img 
+            src=${post.postImageUrl}
+            />
+          </div>
+    
+    `;
+    postContainer.appendChild(postDiv);
+});
+};
+
 fetchAllPosts();
